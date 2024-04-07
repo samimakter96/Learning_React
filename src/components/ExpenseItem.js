52240d2206744c202f8dcdecd89581b48636b6e8
samimakter96 <samimakter9641@gmail.com>
2024-04-07 10:54:21 +0530

    create first custom components in the components folder ExpenseItem.js file

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <p>This is expense item</p>
+      <p>Food - $5</p>
+      <p>Petrol - $20</p>
+      <p>Movie - $10</p>
+    </div>
+  );
+}
+
+export default ExpenseItem;
