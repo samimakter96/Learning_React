8e4ae8c613d4c0a0236b87fcb3ebe7f08f96d141
samimakter96 <samimakter9641@gmail.com>
2024-04-08 22:07:48 +0530

    Spliting into components ExpenseItem to ExpenseDate componentes and App to Expenses components

---
+++ src/components/Expenses.js
@@ -0,0 +1,23 @@
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css'
+
+
+function Expenses(props) {
+  return (
+    <div className='expenses'>
+      {props.expenses.map((expense, index) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            title={expense.title}
+            price={expense.price}
+            location={expense.location}
+          ></ExpenseItem>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Expenses;
